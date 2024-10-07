14da5558bb8c7937d5bfa5f55da316c1aceb38b9
CassioSC <cassio.carneiro@dbccompany.com.br>
2024-10-07 15:45:11 -0300

    código quase feito

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-//@ts-check
+window.addEventListener("DOMContentLoaded", function(){
+
+    const linkedIn = document.getElementById("lknd");
+
+    linkedIn.onclick = function(){
+        window.open("https://google.com.br", "_blank");
+    }
+
+})
